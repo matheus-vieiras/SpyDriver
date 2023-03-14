bc960841cbaf7d9437949fbe3128aed3f77f59ec
Matheus Vieira <matheusvieira.contato@hotmail.com>
2023-03-13 22:11:36 -0300

    adicionando segunda activity

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.helloworld
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -10,11 +12,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnNextScreen = findViewById<Button>(R.id.start_progress)
+
+        fun onClickAqui(view: View) {
+            Toast.makeText(this, "Iniciando...", Toast.LENGTH_SHORT).show()
+        }
+
+        btnNextScreen.setOnClickListener {
+            val i = Intent(this, NextActivity::class.java)
+            startActivity(i)
+        }
+
     }
 
-    fun onClickAqui(view: View) {
-        Toast.makeText(this, "Iniciando...", Toast.LENGTH_SHORT).show()
-    }
 
 }
 
